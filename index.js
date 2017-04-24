6c5b7027205b898280de2bdea260d073d74b3e8d
Sage Wall <sagewall@users.noreply.github.com>
2017-04-24 11:44:54 -0600

    add index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const Twitter = require('twitter');
+
+const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+const term = ' ';
+
+client.stream('statuses/filter', {locations: '-105.4,39.13,-105.05,39.91'}, (stream) => {
+    stream.on('data', (tweet) => {
+        if (tweet.text.toLowerCase().includes(term)) {
+            console.log(tweet.text, tweet.coordinates, tweet.place.name);
+        }
+    });
+
+    stream.on('error', (error) => {
+        console.log(error);
+    });
+});
